150a762c93b15314a4529baf74878d63db66a795
Pandiarajan342015 <pandiarajan342015@gmail.com>
2024-03-30 22:40:10 +0000

    require rename

---
+++ Day6/index.js
@@ -0,0 +1,36 @@
+// const employees = [
+//     {
+//     name: "emp1",
+//     age: 25,
+//     email: "emp1@gmail.com",
+// },
+// {
+// name: "emp2",
+//     age: 25,
+//     email: "emp2@gmail.com",
+// },
+// {
+//     name: "emp3",
+//     age: 25,
+//     email: "emp3@gmail.com",
+// },
+// ];
+// const employeReports = employees
+// for (i = 0; i > employess.length; i++){
+    
+// }
+
+
+var shape = {
+    is2D: true,
+};
+var circle = {
+    radius: 3,
+};
+var rectangle = {
+    length: 1,
+    breadth: 2,
+};
+circle._proto_ = shape;
+}
+}
---
+++ Day7/index.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+// class Person{
+//   constructor(name) {
+//     this.name = name
+    
+//   }
+//   getName() {
+//     return this.name;
+//   }
+// }
+// class Employee extends Person {
+//   constructor(name, id)
+//   super(name);
+//   this.id = id;
+// }
+// getID() {
+//   return this.id
+// }
+// let emp = new employee["pandian", "1010"];
+// console.log(Emp get name: ",emp_getName());
+// console.log(Emp get id: ",emp.get());  
+  
+
+// // class teamHead {
+// //   constructor() {
+// //     console.log("Hi i'm teamHead");
+// //   }
+// //   decisionTaking = true;
+// //   teaching = false;
+// //   training = false;
+// // }
+// // class teamsup extends teamHead {
+// //   constructor() {
+// //     super();
+// //     console.log("Hi i'm teamHead");
+// //   }
+// //   teaching = true;
+// //   trainig = true;
+// // }
+
+// // // // const employees = [
+// // // //     {
+// // // //     name: "emp1",
+// // // //     age: 25,
+// // // //     email: "emp1@gmail.com",
+// // // // },
+// // // // {
+// // // // name: "emp2",
+// // // //     age: 25,
+// // // //     email: "emp2@gmail.com",
+// // // // },
+// // // // {
+// // // //     name: "emp3",
+// // // //     age: 25,
+// // // //     email: "emp3@gmail.com",
+// // // // },
+// // // // ];
+// // // // const employeReports = employees
+// // // // for (i = 0; i > employess.length; i++){
+
+// // // // }
+
+// // // var shape = {
+// // //     is2D: true,
+// // // };
+// // // var circle = {
+// // //     radius: 3,
+// // // };
+// // // var rectangle = {
+// // //     length: 1,
+// // //     breadth: 2,
+// // // };
+// // // circle._proto_ = shape;
+// // // }
+// // // }
