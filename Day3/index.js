b08271fa94857910ccdc239ee30f42d779a17735
Pandiarajan342015 <pandiarajan342015@gmail.com>
2024-03-30 22:29:08 +0000

    json

--- Day3/index.js
+++ Day3/index.js
@@ -1,1 +1,12 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+xhr.onload = function () {
+    var countries = JSON.parse(xhr.responseText);
+    countries.forEach(element => {
+        console.log(countries.name.common + ":" + Country.flag);  
+    });
+        
+});
+xhr.send();
+
 
