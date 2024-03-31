7f635c71f0770f9b4f31cc8f52dd61337167116c
Pandiarajan342015 <pandiarajan342015@gmail.com>
2024-03-31 16:40:53 +0100

    JSON_Task

--- Day3/index.js
+++ Day3/index.js
@@ -1,12 +1,34 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://restcountries.com/v3.1/all", true);
-xhr.onload = function () {
-    var countries = JSON.parse(xhr.responseText);
-    countries.forEach(element => {
-        console.log(countries.name.common + ":" + Country.flag);  
-    });
-        
-});
-xhr.send();
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+// xhr.onload = function () {
+//   //console.log("XMLHTTPRequest -status", xhr.status);
+//   var countries = JSON.parse(xhr.responseText);
+//   //countries.forEach((country) => {
+//   //console.log(country.name.common + ":" + country.flag);
+//   for (i = 0; i < countries.length; i++) {
+//     console.log(countries[i].name.common + " " + countries[i].flag);
+//     console.log("region:", countries[i].region);
+//     console.log("subregion:", countries[i].subregion);
+//     console.log("population:", countries[i].population);
+//   }
+// };
 
+// xhr.send();
 
+let obj1 = { name: "person1", age: 5 };
+let obj2 = { age: 5, name: "person1" };
+function areObjectsEqual(obj1, obj2) {
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (var i = 0; i < key1.length; i++) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+  console.log(areObjectsEqual(obj1, obj2));
+}
+
+//console.log("obj1 keys:", Object.keys(obj1));
+//console.log("obj2 keys:", Object.keys(obj2));
