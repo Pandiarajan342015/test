6edfc283ca178f763279a0008df409417699c098
Pandiarajan342015 <pandiarajan342015@gmail.com>
2024-04-03 00:11:02 +0100

    Program_using anonymous and Arrow function

--- Day4/index.js
+++ Day4/index.js
@@ -6,12 +6,23 @@
 // }
 // totalpurchase();
 
-var totalPurchase = function (){
+var totalPurchase = function () {
   var purchaseAmount = 2000;
   var discount = 500;
-var totalBalance = purchaseAmount - discount;
-return totalBalance;
-}
-console.log(totalPurchase())
-    
-    
+  var totalBalance = purchaseAmount - discount;
+  return totalBalance;
+};
+console.log(totalPurchase());
+
+
+//	Convert all the strings to title caps in a string array//
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (let i = 0; i < str.length; i++) {
+	        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+	    return str.join(' ');
+	}
+console.log(titleCase("full stack development"));
+//
+  
